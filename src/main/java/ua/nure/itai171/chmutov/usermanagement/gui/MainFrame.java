f8d92504c2c0531417ed76978e775e94c6130028
Vertuzen <xXLarsikXx@gmail.com>
2019-11-23 17:41:31 +0200

    Start realization view MainFrame

--- src/main/java/ua/nure/itai171/chmutov/usermanagement/gui/MainFrame.java
+++ src/main/java/ua/nure/itai171/chmutov/usermanagement/gui/MainFrame.java
@@ -1,9 +1,33 @@
 package main.java.ua.nure.itai171.chmutov.usermanagement.gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
 
 public class MainFrame extends JFrame {
     public static void main(String[] args) {
-
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+    private void initialize() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setTitle(Messages.getString("userManagement"));
+        this.setContentPane(getContentPanel());
+    }
+    private JPanel getContentPanel() {
+        if (Objects.isNull(contentPanel)) {
+            contentPanel = new JPanel();
+            contentPanel.setLayout(new BorderLayout());
+            contentPanel.add(getBrowsePanel(), BorderLayout.CENTER);
+        }
+        return contentPanel;
+    }
+    private JPanel getBrowsePanel() {
+        if (Objects.isNull(browsePanel)) {
+            browsePanel = new BrowsePanel(this);
+        }
+        ((BrowsePanel) browsePanel).initTable();
+        return browsePanel;
     }
 }
