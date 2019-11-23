a3498cc7156f770f973b9f38cf234f34779ebad9
Vertuzen <xXLarsikXx@gmail.com>
2019-11-23 17:32:15 +0200

    Add a addon method find

--- src/test/java/ua/nure/itai171/chmutov/usermanagement/gui/MainFrameTest.java
+++ src/test/java/ua/nure/itai171/chmutov/usermanagement/gui/MainFrameTest.java
@@ -1,20 +1,34 @@
 package test.java.ua.nure.itai171.chmutov.usermanagement.gui;
+
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
 import main.java.ua.nure.itai171.chmutov.usermanagement.gui.MainFrame;
 
+import java.awt.*;
+
+import static junit.framework.Assert.assertNotNull;
+
 public class MainFrameTest extends JFCTestCase {
     private MainFrame mainFrame;
 
-    public void setUp(){
+    public void setUp() {
         setHelper(new JFCTestHelper());
         mainFrame = new MainFrame();
         mainFrame.setVisible(true);
     }
-    public void tearDown() throws Exception{
+
+    public void tearDown() throws Exception {
 
 
         mainFrame.setVisible(false);
         TestHelper.cleanUp(this);
+    }
+
+    private Component find(Class componentClass, String name) {
+        NamedComponentFinder finder = new NamedComponentFinder(componentClass, name);
+        finder.setWait(0);
+        Component component = finder.find(mainFrame, 0);
+        assertNotNull("Could not find component ‘" + name + "’", component);
+        return component;
     }
 }
